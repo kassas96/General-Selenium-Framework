ef946e0eb28a81bd182ceb6f43ff9562c1e5b3fe
Kassas <m.a.kassas96@gmail.com>
2025-02-23 00:34:08 +0200

    add the PropertyReader class in General utils

--- EL-Kassas Framework/General_Utils/src/main/java/constants/GlobalConstants.java
+++ EL-Kassas Framework/General_Utils/src/main/java/constants/GlobalConstants.java
@@ -1,4 +1,13 @@
 package constants;
 
 public class GlobalConstants {
+    // File paths
+    public static final String MOBILE_CONFIG_FILE_PATH = "EL-Kassas Framework/Mobile/src/main/resources/mobileConfig.properties";
+    public static final String WEB_CONFIG_FILE_PATH = "EL-Kassas Framework/Web/src/main/resources/webConfig.properties";
+    public static final String API_CONFIG_FILE_PATH = "EL-Kassas Framework/API/src/main/resources/APIConfig.properties";
+    public static final String API_TEST_DATA_PATH = "EL-Kassas Framework/API/src/test/resources/apiTestData.properties";
+    public static final String MOBILE_TEST_DATA_PATH = "EL-Kassas Framework/Mobile/src/test/resources/mobileTestData.properties";
+    public static final String WEB_TEST_DATA_PATH = "EL-Kassas Framework/Web/src/test/resources/apiTestData.properties";
+    public static final String LOGS_PATH = "";
+    public static final String REPORTS_PATH = "";
 }
--- EL-Kassas Framework/General_Utils/src/main/java/reports/PropertyReader.java
+++ EL-Kassas Framework/General_Utils/src/main/java/reports/PropertyReader.java
@@ -1,4 +1,36 @@
 package reports;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static constants.GlobalConstants.*;
+
 public class PropertyReader {
+    private static final Properties properties = new Properties();
+    private static String filePath;
+
+    public static String getProperty(String key,ModuleName moduleName) {
+        switch (moduleName){
+            case api -> filePath=API_CONFIG_FILE_PATH;
+            case web -> filePath=WEB_CONFIG_FILE_PATH;
+            case mobile -> filePath=MOBILE_CONFIG_FILE_PATH;
+            default -> throw new IllegalArgumentException("Invalid module name: " + moduleName);
+        }
+        System.out.println("Loading config file: " + filePath);
+        try {
+            FileInputStream input = new FileInputStream(filePath);
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties.getProperty(key);
+    }
+
+    public enum ModuleName{
+        api,
+        mobile,
+        web
+    }
 }
